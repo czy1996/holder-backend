ca3cc9b4910b9b72bcdbcab2fa8b7bfeb3b0ad94
helion <czy1996@users.noreply.github.com>
2017-08-08 16:13:40 +0800

    history

--- routes/user.py
+++ routes/user.py
@@ -5,8 +5,11 @@ from flask import (
 )
 from models.user import User
 from models.book import Book
+from models.order import Order
 
 from utils import log, json_response
+
+import datetime
 
 main = Blueprint('user', __name__)
 
@@ -45,3 +48,30 @@ def update_cart():
         'cart': data,
     })
     return jsonify(u.json())
+
+
+@main.route('/closeCart')
+def close_cart():
+    u = User.current_user()
+    cart = u.cart
+    o = Order.new(user=u.id, items=cart)
+    return jsonify(o.json())
+
+
+@main.route('/getOrders')
+def get_orders():
+    u = User.current_user()
+    l = Order.find(user=u.id)
+    l = [o.json() for o in l]
+    for order in l:
+        r = []
+        for id, q in order['items'].items():
+            log(id, q)
+            title = Book.get(int(id)).title
+            r.append({
+                'title': title,
+                'quantity': q,
+            })
+        order['items'] = r
+        order['time'] = datetime.datetime.fromtimestamp(order['ct']).strftime("%Y-%m-%d %H:%M:%S")
+    return json_response(l)
