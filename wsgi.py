9397b3395ed895025e62e6c7f9a1240a549cd8b7
helion <czy1996@users.noreply.github.com>
2017-08-23 20:59:11 +0800

    clean debug lines

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,7 @@
 import sys
 from os.path import abspath
 from os.path import dirname
-
-print('***', sys.path)
-
 import app
 
 sys.path.insert(0, abspath(dirname(__file__)))
-print('***', sys.path)
 application = app.app
